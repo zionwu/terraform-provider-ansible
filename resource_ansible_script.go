8cab91d544b81466acf652334e4c3fb7a66ef470
zionwu <zionwu3@gmail.com>
2017-08-16 14:58:37 +0800

    caputure output

--- resource_ansible_script.go
+++ resource_ansible_script.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os/exec"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -29,15 +30,16 @@ func resourceAnsibleScript() *schema.Resource {
 			},
 			"result": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
+				Required: true,
 			},
 		},
 	}
 }
 
+var res []byte
+
 func resourceAnsibleScriptCreate(d *schema.ResourceData, meta interface{}) error {
 	host := d.Get("host").(string)
-	runType := d.Get("type").(string)
 	file := d.Get("file").(string)
 
 	copyStr := fmt.Sprintf("src=%s dest=/tmp/%s", file, file)
@@ -46,16 +48,20 @@ func resourceAnsibleScriptCreate(d *schema.ResourceData, meta interface{}) error
 	if err != nil {
 		return err
 	}
+	logrus.Info(string(res))
 
+	runType := d.Get("type").(string)
 	runStr := fmt.Sprintf("%s /tmp/%s", runType, file)
 	runCmd := exec.Command("ansible", host, "-u", "root", "-a", runStr)
 	res, err = runCmd.Output()
 	if err != nil {
 		return err
 	}
+	logrus.Info(string(res))
+	d.Set("result", string(res))
+	d.SetId("1")
 
-	d.Set("result", res)
-	return nil
+	return resourceAnsibleScriptRead(d, meta)
 
 }
 
